6c5eaf0356bb797dd2c12972ba6099bc2f85d991
Silpharion <xtarokundesu@gmail.com>
2017-10-08 14:24:18 -0700

    Worked out main
    Added menu

--- lab1-currencyClass/main.cpp
+++ lab1-currencyClass/main.cpp
@@ -1,11 +1,14 @@
 /*
 Lab 1
-This program emulates a wallet by adding, removing or emptying the entire contents of the
-wallet with different currencies.
-Ying Tung Wong, Christian Magpantay
+
+This program simulates a wallet during a foreign trip
+by adding or subracting different currencies by the user or
+displaying or emptying the contents of the wallet.
+
+By Ying Tung Wong, Christian Magpantay
 
 Assumptions:
-Default values will start off as zero, then we can only add to the wallet first
+
 
 */
 
@@ -19,25 +22,16 @@ Default values will start off as zero, then we can only add to the wallet first
 #include "c_Yen.h"
 #include "c_Yuan.h"
 #include "wallet.h"
+#include "wallet.cpp"
 
 using namespace std;
 
 /*
 Main
 Psuedocode:
-	Program will ask user which currency they would like to add first
-		ask user how much will be added
-		input validation for program
-			if positive,
-				output, input validation to user
-				output, amount has added
-				lead user to main prompts
+	Program will show the user a series of options for the wallet program
 
-			if negative,
-				output, unable to use negative
-				ask for proper input
-
-	Ask user main prompts; either to add, remove or empty the entire contents of the wallet, or exit program
+	Ask user main prompts; add, remove, display, empty the entire contents of the wallet, or exit program
 		while loop ( while choice is not exit program )
 			if add,
 				ask which currency
@@ -62,6 +56,9 @@ Psuedocode:
 				output amount has removed
 				back to main
 
+			if show,
+				display wallet array
+
 			if empty,
 				print current state of wallet
 				output to user of emptying contents
@@ -74,34 +71,140 @@ Psuedocode:
 
 int main()
 {
-	int currnc, choice;
-	double amount;
+	int currnc, choice, confirm;
+	int whAm, frAm;
+	double totalAm;
+	bool tryAgain = true;
+	Wallet wlet;
 
-	//allow the user to add a maximum of 5 different currency types to the wallet in the form of the base currency array
-	cout << "Which currency would you like to add to your wallet? Input number: " << endl;
-	cout << "(Dollars = 1, Euros = 2, Yen = 3, Rupees = 4, Yuans = 5)";
-	cin >> currnc;
+	//Prompt the user which option he would like to choose
+	//from their wallet.
 
-	//insert into object
-
-	cout << "How much would you like to add of this currency?";
-	cin >> amount;
-
-	cout << "Is " << "'" << amount << "'" << "  the correct amount?";
-
-	//able to add or remove as many currency values for any of the five currency types
-	//able to empty the wallet and print relevant information about the operation
-	cout << "Would you like to add more, remove or empty from your wallet? Enter 0 to exit. " << endl;
-	cout << "(Add = 1, Remove = 2, Empty = 3)";
+	cout << "This wallet program will contain all of your currencies during your trip!" << endl;
+	cout << "Would you like to.." << endl;
+	cout << "1. Add to a specific currency? " << endl;
+	cout << "2. Subtract from a specific currency? " << endl;
+	cout << "3. Show the contents of your wallet? " << endl;
+	cout << "4. Empty the contents of your wallet? " << endl;
+	cout << "5. Exit the program." << endl << endl;
+	cout << "Please enter the number of your choice." << endl;
 	cin >> choice;
 
+	while (choice != 5)
+	{
+		if (choice == 1)
+		{
+			cout << "Please select the type of currency you would like to add." << endl;
+			cout << "1. Dollars" << endl;
+			cout << "2. Euros" << endl;
+			cout << "3. Yen" << endl;
+			cout << "4. Rupees" << endl;
+			cout << "5. Yuan" << endl;
+			cin >> currnc;
 
-	//demonstrate the overloaded subscript operator for the Wallet class
+			if (currnc == 1)
+			{
+				cout << " How many 'dollars' would you like to add?" << endl;
+				cin >> amount;
+				while (tryAgain)
+				{
+					try
+					{
+						//wallet array object
+						//wlet.set[0](amount);
+						tryAgain = false;
+					}
+					catch (Currency::invalidAdd)
+					{
+						cout << "The value of the amount must be a positive value." << endl;
+						cout << " How many 'dollars' would you like to add?" << endl;
+						cin >> amount;
+					}
+				}
 
-	cout << "";
+				cout << "Is " << "'" << amount << "'" << "  the correct amount?" << endl;
+				cout << "Enter 1 for 'Yes' or 2 for 'No'. ";
+				cin >> confirm;
 
-	//output the total value by name of the currencies in the wallet
-	//wallet output array
+				if (confirm == 1)
+				{
+					//wlet.set[](amount);
+					cout << "'" << amount << "'" << " has been added."
+				}
+				else if (confirm == 2)
+				{
+					cout << "How many 'dollars' would you like to add?" << endl;
+					cin >> amount;
+					//Add Input validation
+				}
 
+
+
+			}
+			// I'll repeat the function above for the other currencies
+		}
+		else if (choice == 2)
+		{
+			cout << "Please select the type of currency you would like to subtract." << endl;
+			cout << "1. Dollars" << endl;
+			cout << "2. Euros" << endl;
+			cout << "3. Yen" << endl;
+			cout << "4. Rupees" << endl;
+			cout << "5. Yuan" << endl;
+			cin >> currnc;
+
+			if (currnc == 1)
+			{
+				cout << "How many 'dollars' would you like to subtract?" << endl;
+				cin >> amount;
+				while (tryAgain)
+				{
+					try
+					{
+						//wallet array object
+						//wlet.set[0](amount);
+						tryAgain = false;
+					}
+					catch (Currency::invalidAdd)
+					{
+						cout << "The value of the amount must be a positive value." << endl;
+						cout << " How many 'dollars' would you like to add?" << endl;
+						cin >> amount;
+					}
+				}
+
+
+				cout << "Is " << "'" << amount << "'" << "  the correct amount?" << endl;
+				cout << "Enter 1 for 'Yes' or 2 for 'No'. ";
+				cin >> confirm;
+
+				if (confirm == 1)
+				{
+					//wlet.set[](amount);
+				}
+				else if (confirm == 2)
+				{
+					cout << "How many 'dollars' would you like to add?" << endl;
+					cin >> amount;
+					//Add Input validation
+				}
+			}
+
+			// I'll repeat the function above for the other currencies as well
+		}
+		else if (choice == 3)
+		{
+			//display wallet array object
+		}
+		else if (choice == 4)
+		{
+			//reset wallet array object
+		}
+		else
+			cout << "Invalid input.";
+			cout << "Enter the number of your choice: ";
+			cin >> choice;
+
+	}
 
 }
